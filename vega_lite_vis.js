80600d934e5d97079b91e677094e58e716477481
Jingyi-007 <xxjingyixx@gmail.com>
2026-05-06 16:50:37 +0800

    Added line chart

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,19 +1,27 @@
+// Section 1 - World Choropleth Map
 var choroplethMap = "js/choropleth_map.vg.json";
 vegaEmbed("#choropleth_map", choroplethMap)
   .then(function(result) {})
   .catch(console.error);
 
+// Section 2 - Bar Chart
 var barChart = "js/bar_chart.vg.json";
 vegaEmbed("#bar_chart", barChart)
   .then(function(result) {})
   .catch(console.error);
 
+// Section 3 - Malaysia State Choropleth Map
 var malaysiaMap = "js/malaysia_choropleth.vg.json";
 vegaEmbed("#malaysia_map", malaysiaMap)
   .then(function(result) {})
   .catch(console.error);
 
+// Section 4 - Heatmap
 var heatmap = "js/heatmap.vg.json";
 vegaEmbed("#heatmap", heatmap)
   .then(function(result) {})
   .catch(console.error);
+
+// Section 5 - Enrolment Trend Line Chart
+var lineChart = "js/line_chart.vg.json";
+vegaEmbed("#line_chart", lineChart).then(function(result) {}).catch(console.error);
